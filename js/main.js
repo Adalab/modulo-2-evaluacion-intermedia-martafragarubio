9d78a9af114b9f16ccac178f9b17e095b603b9ae
Marta Fraga Rubio <martafragarubio@gmail.com>
2021-05-12 09:54:00 +0200

    start

---
+++ js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+const inputNumber = document.querySelector("js-number");
+const buttonClick = document.querySelector(".js-button");
+
+/*function generateRandomNumber() {
+    // 0 .. 100
+    const number = Math.round(     Math.random() * 100    );
+    return number;
+}
+
+
+function getRandomNumber(100) {
+
+  return Math.ceil(Math.random() * 100);
+}*/
